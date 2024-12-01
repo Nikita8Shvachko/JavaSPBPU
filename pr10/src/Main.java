0b15405664ae2c98838e68a01ad88edfaf6cb7b1
Nikita Shvachko <228nikitalik@gmail.com>
2024-12-01 21:27:27 +0300

    Co,plete pr10

---
+++ pr10/src/Main.java
@@ -0,0 +1,10 @@
+import package1.ParentClass;
+import package2.ChildClass;
+import package3.Mainp;
+public class Main {
+
+    public static void main(String[] args) {
+        Mainp.main(args);
+
+    }
+}
