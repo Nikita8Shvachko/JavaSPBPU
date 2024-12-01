718dbee261d4b08ebc0d4473d6d5d1f8ea67e6de
Nikita Shvachko <228nikitalik@gmail.com>
2024-12-01 21:02:38 +0300

    Complete pr8

---
+++ pr8/src/Main.java
@@ -0,0 +1,64 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+//Практика #1: Тривиальные примеры бесконечных циклов
+        {
+//        Бесконечный цикл while:
+//            while (true) {
+//                System.out.println("Бесконечный цикл while");
+//            }
+        }
+
+        {
+//            Бесконечный цикл do-while
+//            int value = 1;
+//            while (value > 0) { // Условие всегда истинно
+//                value-=1;
+//                System.out.println("Бесконечный цикл while");
+//            }
+        }
+
+        {
+//            do {
+//                System.out.println("Бесконечный цикл do-while");
+//            } while (true);
+        }
+//Практика #2: Примеры использования операторов break и continue
+        {
+//            Пример 1: Использование break для выхода из цикла
+//            for (int i = 0; i < 10; i++) {
+//                if (i == 5) {
+//                    break; // Прерывание цикла при достижении i == 5
+//                }
+//                System.out.println("i = " + i);
+//            }
+//            System.out.println("Цикл завершен");
+
+        }
+
+        {
+//            Пример 2: Использование continue для пропуска итерации
+//            for (int i = 0; i < 10; i++) {
+//                if (i % 2 == 0) {
+//                    continue; // Пропуск итерации для четных чисел
+//                }
+//                System.out.println("i = " + i);
+//            }
+        }
+//        Использование break и continue в while цикле
+//        int i = 0;
+//        while (i < 10) {
+//            if (i == 7) {
+//                break; // Прерывание цикла при достижении i == 7
+//            }
+//            if (i % 2 == 0) {
+//                i++;
+//                continue; // Пропуск итерации для четных чисел
+//            }
+//            System.out.println("i = " + i);
+//            i++;
+    }
+
+
+}
